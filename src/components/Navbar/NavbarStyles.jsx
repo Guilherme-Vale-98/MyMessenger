33fcb2198c0f7887910b869d3e23ed162d12b23f
Guilherme Vale <guilherme.vale98@gmail.com>
2023-03-21 13:39:37 -0300

    add Navbar

--- src/components/Navbar/NavbarStyles.jsx
+++ src/components/Navbar/NavbarStyles.jsx
@@ -1,16 +1,40 @@
+
 import styled from 'styled-components';
+
 
 export const NavContainer = styled.nav`
     display: flex;
     width: 100%;
     background-color: #1c1d1c;
     justify-content: space-between;
+    align-items: center;
+    height: 4rem;
+    h2>*{
+        all:unset;
+        color: rgb(222, 216, 209);
+        margin-left: 3rem;
+        font-weight: 100;
+        cursor: pointer;
+        &:hover{
+            color: gray;
+        }    
+    }
+    
 `
 
-
 export const NavButtons = styled.div`
-    background-color: darkblue;
-    text-align: center;
-    
+    margin-right: 3rem;
+    span{
+        padding: 1rem;        
+    }
+    span>*{
+        all: unset;
+        color: rgb(222, 216, 209);
+        cursor:pointer;
+        &:hover{
+            color: gray;
+        }   
+
+    }
 
 `;
