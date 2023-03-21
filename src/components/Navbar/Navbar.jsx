9e47c882d11b209d87c014dec2f33a80b9d09135
Guilherme Vale <guilherme.vale98@gmail.com>
2023-03-21 13:39:37 -0300

    add Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,16 @@ import {NavButtons, NavContainer} from './NavbarStyles';
 const Navbar = () => {
   return (
     <NavContainer>
-        <h3>
+        <h2>
             <Link to="/">MyMessenger</Link>
-        </h3>
+        </h2>
         <NavButtons>
-            <Link to='/register'>Cadastre-se</Link>
-            <Link to='/login'>Entre</Link>
+            <span>
+                <Link className='a' to='/register'>Cadastre-se</Link>
+            </span>
+            <span>
+                <Link to='/login'>Entre</Link>
+            </span>
         </NavButtons>           
     </NavContainer>
   )
