02c2b7890063d5af69efa55a7f37f8bb462d6aa6
Guilherme Vale <guilherme.vale98@gmail.com>
2023-03-21 12:00:48 -0300

    install styled components

---
+++ src/components/Navbar/NavbarStyles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.nav`
+    display: flex;
+    width: 100%;
+    background-color: #1c1d1c;
+    justify-content: space-between;
+`
+
+
+export const NavButtons = styled.div`
+    background-color: darkblue;
+    text-align: center;
+    
+
+`;
