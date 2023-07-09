dcc57bd7eb8eb236100d2c9a1c400a0bf7315bdc
Guilherme Vale <guilherme.vale98@gmail.com>
2023-07-09 10:51:34 -0300

    added chat container and user select feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,11 @@ import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { auth, db } from '../utils/firebase.utils';
 import User from '../components/User/User';
-import { UserContacts, UserContainer} from '../components/User/User.styles';
+import { UserContacts, UserContainer, ChatContainer, MessageContainer, AppContainer} from '../components/User/User.styles';
 
 function Home() {
   const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("")
 
   useEffect(() =>{
     const usersRef = collection(db, "users");
@@ -17,15 +18,29 @@ function Home() {
       })
       setUsers(users);
     })
-
     return ()=>unsub();
   }
   ,[])
 
+  const selectUser = (user) =>{
+    setChat(user)
+    console.log(user)
+  }
   return (
-    <UserContainer>
+    <AppContainer>
+      <UserContainer>
       <span>Contatos</span>
-      {users.map(user => <User user={user}/> )}</UserContainer>
+      {users.map(user => <User key={user.uid} user={user} selectUser={selectUser}/> )}
+      </UserContainer>
+      <ChatContainer>
+        {chat? (<MessageContainer>
+          {chat.name}
+        </MessageContainer>):(<MessageContainer>
+          <h3>Select a user to start a conversation</h3>
+        </MessageContainer>)}
+      </ChatContainer>
+    </AppContainer>
+    
   )
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -42,9 +42,7 @@ const Profile = () => {
                     setImage("");
                 } catch (error) {
                     console.log(error.message)
-                }
-                
-                
+                }               
             }
 
             uploadImg();
